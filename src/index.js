fc5f91ad7f820ac07be46d5ade168b958ea30cac
Tautik Agrahari <tautikagrahari@gmail.com>
2023-01-08 13:29:17 +0530

    added basic dotenv setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
-require("dotenv").config();
+const { PORT } = require("./config/serverConfig");
 
 const setupAndStartServer = async () => {
   const app = express();
-  const PORT = 3000; //-> put inside env for data hiding
   app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
   });
