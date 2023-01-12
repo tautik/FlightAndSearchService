fcb0ab281348ff012e0a12c7ea4d742158eeacca
Tautik Agrahari <tautikagrahari@gmail.com>
2023-01-12 11:38:32 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,23 +1,56 @@
 const { City } = require("../models/index");
 
 class CityRepository {
-  async createCity({ cityName }) {
+  async creteCity({ name }) {
+    //we will get {name:___,},destructuring to directly access name key
     try {
-      const city = await City.create({ cityName });
+      const city = await City.create(name);
+      return city;
     } catch (err) {
-      throw err;
+      console.log("Something went wrong in the repository layer");
+      throw { err };
     }
   }
 
-  async deleteCity({ cityId }) {
+  async deleteCity(cityId) {
+    //here we get only id thus no destructuring of object
     try {
       await City.destroy({
         where: {
           id: cityId,
         },
       });
+      return true;
     } catch (err) {
-      throw err;
+      console.log("Something went wrong in the repository layer");
+      throw { err };
+    }
+  }
+
+  async updateCity(cityId, data) {
+    // id to be deleted and {data to be updated}
+    try {
+      await City.update(
+        { data },
+        {
+          where: {
+            id: cityId,
+          },
+        }
+      );
+    } catch (err) {
+      console.log("Something went wrong in the repository layer");
+      throw { err };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId); //returns object if found else null
+      return city;
+    } catch (err) {
+      console.log("Something went wrong in the repository layer");
+      throw { err };
     }
   }
 }
